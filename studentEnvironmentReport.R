532a0beaa1d3e4ee4b6c5761f0d919b5a9669b3b
raytang1758 <61776013+raytang1758@users.noreply.github.com>
2020-04-15 16:39:41 +0800

    20200415

---
+++ studentEnvironmentReport.R
@@ -0,0 +1,16 @@
+if(!require("readr")){
+  install.packages("readr")
+}
+sink("report.txt")
+Sys.getenv()
+Sys.getlocale()
+readr::locale()
+sessionInfo()
+version
+sink()
+warning(
+  'Please upload the following file: \n',
+  file.path(getwd(),"report.txt"),
+  '\nto the following website:\n',
+  'https://forms.gle/vPoQ1MC4cNEJuNqM8'
+)
